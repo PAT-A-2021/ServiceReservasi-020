ac1f015cd5143174f5d83ced5ba2e4942c53986e
FatkhiAlauddin <cupetongspeed12@gmail.com>
2021-12-27 19:13:33 +0700

    Praktikum11

--- ServiceReservasi-020/IService1.cs
+++ ServiceReservasi-020/IService1.cs
@@ -12,24 +12,35 @@ namespace ServiceReservasi_020
     public interface IService1
     {
         [OperationContract]
-        string pemesanan(string IDReservasi, string NamaCustomer, string NoTelepon, int JumlahPemesanan, string IDLokasi); //method proses input data
+        string pemesanan(string IDReservasi, string NamaCustomer, string NoTelepon, int JumlahPemesanan, string IDLokasi); // Method proses input data
         [OperationContract]
         string editPemesanan(string IDReservasi, string NamaCustomer, string No_telepon);
         [OperationContract]
         string deletePemesanan(string IDReservasi);
         [OperationContract]
-        List<CekLokasi> ReviewLokasi();//menampilkan data yang ada di database (select all) dengan menampilkan isi dari yang ada contract
+        List<CekLokasi> ReviewLokasi();// Menampilkan data yang ada di database (select all) dengan menampilkan isi dari yang ada contract
         [OperationContract]
-        List<DetailLokasi> DetailLokasi();//menampilkan detail lokasi
+        List<DetailLokasi> DetailLokasi();// Menampilkan detail lokasi
         [OperationContract]
         List<Pemesanan> Pemesanan();
+
+        [OperationContract]
+        string Login(string username, string password);
+        [OperationContract]
+        string Register(string username, string password, string kategori);
+        [OperationContract]
+        string UpdateRegister(string username, string password, string kategori, int id);
+        [OperationContract]
+        string DeleteRegister(string username);
+        [OperationContract]
+        List<DataRegister> DataRegist();
     }
 
     [DataContract]
-    public class CekLokasi //daftar lokasi
+    public class CekLokasi // Daftar lokasi
     {
         [DataMember]
-        public string IDLokasi { get; set; }//variabel dari public class
+        public string IDLokasi { get; set; }// Variabel dari public class
         [DataMember]
         public string NamaLokasi { get; set; }
         [DataMember]
@@ -37,10 +48,10 @@ namespace ServiceReservasi_020
     }
 
     [DataContract]
-    public class DetailLokasi //menampilkan detail lokasi
+    public class DetailLokasi // Menampilkan detail lokasi
     {
         [DataMember]
-        public string IDLokasi { get; set; } //variabel dari public class
+        public string IDLokasi { get; set; } // Variabel dari public class
         [DataMember]
         public string NamaLokasi { get; set; }
         [DataMember]
@@ -50,17 +61,30 @@ namespace ServiceReservasi_020
     }
 
     [DataContract]
-    public class Pemesanan //create
+    public class Pemesanan // Create
     {
         [DataMember]
         public string IDReservasi { get; set; }
         [DataMember]
-        public string NamaCustomer { get; set; } //method
+        public string NamaCustomer { get; set; } // Method
         [DataMember]
         public string NoTelepon { get; set; }
         [DataMember]
         public int JumlahPemesanan { get; set; }
         [DataMember]
         public string Lokasi { get; set; }
+    }
+
+    [DataContract]
+    public class DataRegister
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+        [DataMember(Order = 2)]
+        public string username { get; set; }
+        [DataMember(Order = 3)]
+        public string password { get; set; }
+        [DataMember(Order = 4)]
+        public string kategori { get; set; }
     }
 }
--- ServiceReservasi-020/Service1.cs
+++ ServiceReservasi-020/Service1.cs
@@ -15,6 +15,36 @@ namespace ServiceReservasi_020
         SqlConnection connection;
         SqlCommand com; //untuk mengkoneksikan database ke visual studio
 
+        public List<DataRegister> DataRegist()
+        {
+            List<DataRegister> list = new List<DataRegister>();
+            try
+            {
+                string sql = "select ID_Login, Username, Password, Kategori from Login";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    DataRegister data = new DataRegister();
+                    data.id = reader.GetInt32(0);
+                    data.username = reader.GetString(1);
+                    data.password = reader.GetString(2);
+                    data.kategori = reader.GetString(3);
+                    list.Add(data);
+                }
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+
+            return list;
+        }
+
         public string deletePemesanan(string IDReservasi)
         {
             string a = "gagal";
@@ -22,7 +52,7 @@ namespace ServiceReservasi_020
             try
             {
                 string sql = "delete from dbo.Pemesanan where ID_reservasi = '" + IDReservasi + "'";
-                connection = new SqlConnection(constring); //fungsi konek ke database
+                connection = new SqlConnection(constring); // Fungsi konek ke database
                 com = new SqlCommand(sql, connection);
                 connection.Open();
                 com.ExecuteNonQuery();
@@ -38,28 +68,59 @@ namespace ServiceReservasi_020
             return a;
         }
 
-        public List<DetailLokasi> DetailLokasi()
+        public string DeleteRegister(string username)
         {
-            List<DetailLokasi> LokasiFull = new List<DetailLokasi>(); //proses untuk mendeklarasikan nama list yang telah dibuat dengan nama baru
             try
             {
-                string sql = "select ID_lokasi, Nama_lokasi, Deskripsi_full, Kuota from dbo.Lokasi"; //deklarasi query
-                connection = new SqlConnection(constring); //fungsi untuk koneksi ke database
-                com = new SqlCommand(sql, connection); //proses execute query
-                connection.Open(); //membuka koneksi
-                SqlDataReader reader = com.ExecuteReader(); //menampilkan data query
+                int id = 0;
+                string sql = "select ID_Login from dbo.Login where Username = '" + username + "'";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    id = reader.GetInt32(0);
+                }
+
+                connection.Close();
+                string sql2 = "delete from Login where ID_Login = " + id + "";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql2, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                return "sukses";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        public List<DetailLokasi> DetailLokasi()
+        {
+            List<DetailLokasi> LokasiFull = new List<DetailLokasi>(); // Proses untuk mendeklarasikan nama list yang telah dibuat dengan nama baru
+            try
+            {
+                string sql = "select ID_lokasi, Nama_lokasi, Deskripsi_full, Kuota from dbo.Lokasi"; // Deklarasi query
+                connection = new SqlConnection(constring); // Fungsi untuk koneksi ke database
+                com = new SqlCommand(sql, connection); // Proses execute query
+                connection.Open(); // Membuka koneksi
+                SqlDataReader reader = com.ExecuteReader(); // Menampilkan data query
                 while (reader.Read())
                 {
                     /* Nama Class */
-                    DetailLokasi data = new DetailLokasi(); //deklarasi data dengan mengambil satu per satu data dari database
-                    //bentuk array
+                    DetailLokasi data = new DetailLokasi(); // Deklarasi data dengan mengambil satu per satu data dari database
+                    // Bentuk array
                     data.IDLokasi = reader.GetString(0); // 0 adalah index, terdapat dikolom ke berapa di string sql diatas
                     data.NamaLokasi = reader.GetString(1);
                     data.DeskripsiFull = reader.GetString(3);
                     data.Kuota = reader.GetInt32(4);
-                    LokasiFull.Add(data); //mengumpulkan data yang awalnya dari array
+                    LokasiFull.Add(data); // Mengumpulkan data yang awalnya dari array
                 }
-                connection.Close(); //untuk menutup akses ke database
+                connection.Close(); // Untuk menutup akses ke database
             }
             catch (Exception ex)
             {
@@ -77,7 +138,7 @@ namespace ServiceReservasi_020
             try
             {
                 string sql = "update dbo.Pemesanan set Nama_customer = '" + NamaCustomer + "', No_telepon = '" + No_telepon + "' where ID_reservasi = '" + IDReservasi + "'";
-                connection = new SqlConnection(constring); //fungsi konek ke database
+                connection = new SqlConnection(constring); // Fungsi konek ke database
                 com = new SqlCommand(sql, connection);
                 connection.Open();
                 com.ExecuteNonQuery();
@@ -93,20 +154,37 @@ namespace ServiceReservasi_020
             return a;
         }
 
+        public string Login(string username, string password)
+        {
+            string kategori = "";
+
+            string sql = "select kategori from login where Username = '" + username + "' and Password = '" + password + "'";
+            connection = new SqlConnection(constring);
+            com = new SqlCommand(sql, connection);
+            connection.Open();
+            SqlDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                kategori = reader.GetString(0);
+            }
+
+            return kategori;
+        }
+
         public string pemesanan(string IDReservasi, string NamaCustomer, string NoTelepon, int JumlahPemesanan, string IDLokasi)
         {
             string a = "gagal";
             try
             {
                 string sql = "insert into dbo.Pemesanan values ('" + IDReservasi + "', '" + NamaCustomer + "', '" + NoTelepon + "', '" + JumlahPemesanan + "', '" + IDLokasi + "')"; // Petik 1 untuk menyatakan varchar, petik 2 menyatakan integer
-                connection = new SqlConnection(constring); //fungsi konek ke database
+                connection = new SqlConnection(constring); // Fungsi konek ke database
                 com = new SqlCommand(sql, connection);
                 connection.Open();
                 com.ExecuteNonQuery();
                 connection.Close();
 
                 string sql2 = "update dbo.Lokasi set Kuota = Kuota - " + JumlahPemesanan + "where ID_lokasi = '" + IDLokasi + "'";
-                connection = new SqlConnection(constring); //fungsi konek ke database
+                connection = new SqlConnection(constring); //Fungsi konek ke database
                 com = new SqlCommand(sql2, connection);
                 connection.Open();
                 com.ExecuteNonQuery();
@@ -123,30 +201,30 @@ namespace ServiceReservasi_020
 
         public List<Pemesanan> Pemesanan()
         {
-            List<Pemesanan> pemesanans = new List<Pemesanan>(); //proses untuk mendeklarasi nama list yang telah dibuat dengan fungsi List
+            List<Pemesanan> pemesanans = new List<Pemesanan>(); // Proses untuk mendeklarasi nama list yang telah dibuat dengan fungsi List
 
             try
             {
                 string sql = "select ID_reservasi, Nama_customer, No_telepon, Jumlah_pemesanan, Nama_lokasi from dbo.Pemesanan p join dbo.Lokasi l on p.ID_lokasi = l.ID_lokasi";
-                connection = new SqlConnection(constring); //fungsi konek ke database
-                com = new SqlCommand(sql, connection); //proses execute query
+                connection = new SqlConnection(constring); // Fungsi konek ke database
+                com = new SqlCommand(sql, connection); // Proses execute query
                 connection.Open();
-                SqlDataReader reader = com.ExecuteReader(); //menampilkan data query
+                SqlDataReader reader = com.ExecuteReader(); // Menampilkan data query
                 while (reader.Read())
                 {
-                    //Nama class
-                    Pemesanan data = new Pemesanan(); //deklarasi data dengan mengambil satu per satu dari database
+                    // Nama class
+                    Pemesanan data = new Pemesanan(); // Deklarasi data dengan mengambil satu per satu dari database
 
-                    //Bentuk array
+                    // Bentuk array
                     data.IDReservasi = reader.GetString(0); // 0 itu index, ada dikolom keberapa di string sql diatas
                     data.NamaCustomer = reader.GetString(1);
                     data.NoTelepon = reader.GetString(2);
                     data.JumlahPemesanan = reader.GetInt32(3);
                     data.Lokasi = reader.GetString(4);
-                    pemesanans.Add(data); //mengumpulkan data yang awalnya dari array
+                    pemesanans.Add(data); // Mengumpulkan data yang awalnya dari array
                 }
 
-                connection.Close(); //menutup akses ke database
+                connection.Close(); // Menutup akses ke database
             }
             catch (Exception ex)
             {
@@ -156,9 +234,47 @@ namespace ServiceReservasi_020
             return pemesanans;
         }
 
+        public string Register(string username, string password, string kategori)
+        {
+            try
+            {
+                string sql = "insert into Login values ('" + username + "', '" + password + "', '" + kategori + "')";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                return "sukses";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
         public List<CekLokasi> ReviewLokasi()
         {
             throw new NotImplementedException();
+        }
+
+        public string UpdateRegister(string username, string password, string kategori, int id)
+        {
+            try
+            {
+                string sql2 = "update Login SET Username = '" + username + "', Password = '" + password + "', Kategori = '" + kategori + "' where ID_Login = " + id + "";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql2, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                return "sukses";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
         }
     }
 }
